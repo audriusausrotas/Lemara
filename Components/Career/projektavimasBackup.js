ccc7950d6debfce99778418b3cadf32ba219234e
sopuku <sopuku@gmail.com>
2024-10-30 17:17:26 +0200

    updated 2

---
+++ Components/Career/projektavimasBackup.js
@@ -0,0 +1,50 @@
+// import Head from "next/head";
+// import dynamic from "next/dynamic";
+// import React, { Suspense } from "react";
+// import * as prismic from "@prismicio/client";
+// import sm from "../../sm.json";
+// import Footer from "../../Components/Ui/Footer";
+
+// const DefaultPage = dynamic(() => import("../../Components/Components/DefaultPage"), {
+//   suspense: true,
+// });
+
+// export default function Design({ page, foot }) {
+//   return (
+//     <>
+//       <Head>
+//         <title>{page.data.meta_title}</title>
+//         <meta name="description" content={page.data.meta_description} />
+//       </Head>
+//       <Suspense>
+//         <DefaultPage
+//           name="design"
+//           src={page.data.image.url}
+//           alt={page.data.image.alt}
+//           heading={page.data.title}
+//           text={page.data.text}
+//           w={page.data.image.dimensions.width}
+//           h={page.data.image.dimensions.height}
+//           bg={page.data.background_color}
+//           color={page.data.text_color}
+//           bgTexture={page.data.background_texture.url}
+//           fallback={`Loading...`}
+//         />
+//         <Footer data={foot.data} />
+//       </Suspense>
+//     </>
+//   );
+// }
+
+// export async function getStaticProps({ locale }) {
+//   const client = prismic.createClient(sm.apiEndpoint);
+//   const page = await client.getByUID("design", "design", { lang: locale });
+//   const foot = await client.getByUID("footer", "footer", { lang: locale });
+
+//   return {
+//     props: {
+//       page,
+//       foot,
+//     },
+//   };
+// }
--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -6,11 +6,11 @@ export default async (req, res) => {
     { url: "/", changefreq: "monthly", priority: 0.8 },
     { url: "/paslaugos/tekinimas", changefreq: "monthly", priority: 0.8 },
     { url: "/paslaugos/frezavimas", changefreq: "monthly", priority: 0.8 },
-    { url: "/paslaugos/projektavimas", changefreq: "monthly", priority: 0.8 },
+    // { url: "/paslaugos/projektavimas", changefreq: "monthly", priority: 0.8 },
     { url: "/paslaugos/bendra", changefreq: "monthly", priority: 0.8 },
     { url: "/calculator", changefreq: "monthly", priority: 0.8 },
     { url: "/irengimai", changefreq: "monthly", priority: 0.8 },
-    { url: "/karjera", changefreq: "monthly", priority: 0.8 },
+    // { url: "/karjera", changefreq: "monthly", priority: 0.8 },
     { url: "/kontaktai", changefreq: "monthly", priority: 0.8 },
     { url: "/galerija", changefreq: "monthly", priority: 0.8 },
     { url: "/privatumas", changefreq: "monthly", priority: 0.8 },
@@ -19,8 +19,8 @@ export default async (req, res) => {
   res.writeHead(200, {
     "Content-Type": "application/xml",
   });
-  const xmlString = await streamToPromise(
-    Readable.from(links).pipe(stream)
-  ).then((data) => data.toString());
+  const xmlString = await streamToPromise(Readable.from(links).pipe(stream)).then((data) =>
+    data.toString()
+  );
   res.end(xmlString);
 };
