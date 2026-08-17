5691334dbaec2ac6f3d2d5d21e5628f7732c0cb8
sopuku <sopuku@gmail.com>
2026-08-17 20:56:55 +0300

    updating webhook. again

--- pages/api/prismic-webhook.js
+++ pages/api/prismic-webhook.js
@@ -3,6 +3,10 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
+  if (req.body.secret !== process.env.PRISMIC_WEBHOOK_SECRET) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
   try {
     const response = await fetch(
       `https://api.github.com/repos/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}/dispatches`,
